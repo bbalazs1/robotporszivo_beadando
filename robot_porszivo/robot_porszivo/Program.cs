b74dd127b4d84d86c5efcd7ba51cae31fba47c99
bbalazs1 <b3ligbal@vasvari.org>
2026-01-04 15:38:25 +0100

    Meretek bekerese
    Added user input for row and column counts with validation.

--- robot_porszivo/robot_porszivo/Program.cs
+++ robot_porszivo/robot_porszivo/Program.cs
@@ -10,6 +10,26 @@ namespace robot_porszivo
     {
         static void Main(string[] args)
         {
+            Random veletlen = new Random();
+
+            int sorSzam, oszlopSzam;
+        
+            do
+            {
+                Console.Write("Sorok szama (20-30): ");
+                sorSzam = int.Parse(Console.ReadLine());
+                Console.Write("Oszlopok szama (20-30): ");
+                oszlopSzam = int.Parse(Console.ReadLine());
+            }
+            while (sorSzam < 20 || sorSzam > 30 ||
+                   oszlopSzam < 20 || oszlopSzam > 30 ||
+                   sorSzam == oszlopSzam);
+            
+            char[,] lakas = new char[sorSzam, oszlopSzam];
+            
+            int koszSzam = 0;
+            int szabadSzam = 0;
         }
     }
 }
+
